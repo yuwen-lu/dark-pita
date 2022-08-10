72248c31bb2d2d4027a1f0d5391fdf5ee3a9b988
chaochaooo <373930051@qq.com>
2022-08-10 18:12:37 +0800

    Update README

--- src/contents/components/amazon/disguised_ads/disguised_ads_hide.vue
+++ src/contents/components/amazon/disguised_ads/disguised_ads_hide.vue
@@ -47,11 +47,11 @@ export default {
   mounted() {
     this.emitter.on('buy_now_hide', (massage) => {
       if (massage === 'on') {
-        console.log('disguised ads hide on');
+        console.log('buy now hide on');
         this.target = document.getElementById('buyNow_feature_div');
         this.remove(this.target);
       } else if (massage === 'off') {
-        console.log('disguised ads hide off');
+        console.log('buy now hide off');
         this.recover(this.target);
       }
     });
