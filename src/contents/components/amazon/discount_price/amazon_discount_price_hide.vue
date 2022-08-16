7dc861300b3d73ceb2362381e04258a8b85e1392
Yuewen <yuewenkapa@gmail.com>
2022-08-16 14:29:34 -0400

    amazon-discount not hide all

--- src/contents/components/amazon/discount_price/amazon_discount_price_hide.vue
+++ src/contents/components/amazon/discount_price/amazon_discount_price_hide.vue
@@ -5,25 +5,36 @@
 export default {
   data() {
     return {
-      target: document.querySelectorAll('[id^=apex_desktop]')[0]
+      target: document.querySelectorAll("[id^=apex_desktop]")[0],
+      originalContent:
+        document.querySelectorAll("[id^=apex_desktop]")[0].innerHTML,
+      price: document.querySelector(".a-offscreen"),
     };
   },
   methods: {},
   mounted() {
-    this.emitter.on('amazon_discount_price_hide', (message) => {
-      this.target = document.querySelectorAll('[id^=apex_desktop]')[0];
+    this.emitter.on("amazon_discount_price_hide", (message) => {
+      this.target = document.querySelectorAll("[id^=apex_desktop]")[0];
 
-      if (message === 'on') {
-        console.log('amazon discount price hide on');
-        this.target.style.display = 'none';
-      } else if (message === 'off') {
-        console.log('amazon discount price hide off');
-        this.target.style.display = 'visible';
+      if (message === "on") {
+        console.log("amazon discount price hide on");
+        // the line below is our original method
+        // this.target.style.display = "none";
+        // show the ultimate price only, not other marketing jargons
+        document.querySelector("#apex_desktop").textContent =
+          document.querySelector(".a-offscreen").textContent;
+        // apply tailwind css
+        document.querySelector("#apex_desktop").classList.add("text-2xl");
+        this.target.style.color = "rgb(159,47,47)";
+      } else if (message === "off") {
+        console.log("amazon discount price hide off");
+        this.target.style.display = "visible";
+        // document.querySelector("#apex_desktop") = this.target;
       }
 
-      this.$emit('update');
+      this.$emit("update");
     });
-  }
+  },
 };
 </script>
 <style lang=""></style>
