b6d1909bc15edea4d70e9cb6b44315a6ca100b5c
Yuwen <yuwenlu@andrew.cmu.edu>
2022-08-19 01:00:50 -0400

    Fix facebook reels hide issue

--- src/contents/components/facebook/reels/facebook_reels_hide.vue
+++ src/contents/components/facebook/reels/facebook_reels_hide.vue
@@ -3,47 +3,14 @@
 </template>
 <script>
 export default {
-    data() { },
-    methods: {
-        remove(selectors) {
-            selectors.removeNode = [];
-            if (selectors.length != undefined) {
-                let len = selectors.length;
-                for (let i = 0; i < len; i++) {
-                    selectors.removeNode.push({
-                        parent: selectors[i].parentNode,
-                        inner: selectors[i].outerHTML,
-                        next: selectors[i].nextSibling
-                    });
-                }
-                for (let i = 0; i < len; i++)
-                    selectors[i].parentNode.removeChild(selectors[i]);
-            } else {
-                selectors.removeNode.push({
-                    parent: selectors.parentNode,
-                    inner: selectors.outerHTML,
-                    next: selectors.nextSibling
-                });
-                selectors.parentNode.removeChild(selectors);
-            }
-        },
-        recover(selectors) {
-            let len = selectors.removeNode.length;
-            for (let i = 0; i < len; i++) {
-                let node = selectors.removeNode[i];
-                if (node.next == null) node.parent.innerHTML += node.inner;
-                else {
-                    let div = document.createElement('div');
-                    div.innerHTML = node.inner;
-                    node.parent.insertBefore(div.childNodes[0], node.next);
-                }
-            }
-        }
+    data() {
+        return {
+            target: null,
+            reelsIsHide: false,
+        };
     },
-    mounted() {
-        this.emitter.on('facebook_reels_hide', (message) => {
-            console.log("facebook_reels_hide message: ", message);
-
+    methods: {
+        getTarget() {
             let element;
             // look for the reels component
             var retrievedHtmls = document.getElementsByTagName("span");
@@ -56,27 +23,54 @@ export default {
                         if (element.parentElement == null) break;
                         element = element.parentElement;
                     }
-                    console.log("element", element);
                 }
             }
+            if (element === null || element === undefined) {
+                console.log("facebook_reels_hide: cannot find reels component");
+            } else {
+                console.log("facebook_reels_hide retrieved element", element);
+                this.target = element;
+            }
+        }
+    },
+    mounted() {
+
+        window.addEventListener('scroll', () => {
+            console.log("scroll from facebook_reels_hide, reelsIsHide: " + this.reelsIsHide);
+            if (this.reelsIsHide) {
+                this.getTarget();
+                if (this.target !== null && this.target !== undefined) {
+                    this.target.style.visibility = 'hidden';
+                    console.log("Removed facebook reels: " + this.target.innerHTML);
+                } else {
+                    console.log("Message on, but cannot find target element for facebook reels");
+                }  
+            }
+        });
+
+        this.emitter.on('facebook_reels_hide', (message) => {
+            console.log("facebook_reels_hide message: ", message);
+
+            this.getTarget();
 
             if (message === 'on') {
+                this.reelsIsHide = true;
                 console.log('facebook reels content hide on');
                 this.sendAction(1, 'toggle facebook_reels_hide');
-                console.log("Trying to remove facebook reels, target: " + element.innerHTML);
+                this.getTarget();
 
-                if (element != null && element !== undefined) {
-                    this.target = element;
-                    this.remove(this.target)
-                    console.log("Removed facebook reels: " + element.innerHTML);
+                if (this.target !== null && this.target !== undefined) {
+                    this.target.style.visibility = "hidden";
+                    console.log("Removed facebook reels: " + this.target.innerHTML);
                 } else {
                     console.log("Message on, but cannot find target element for facebook reels");
                 }                
             } else if (message === 'off') {
+                this.reelsIsHide = false;
                 console.log('facebook reels content hide off');
                 this.sendAction(0, 'toggle facebook_reels_hide');
-                if (this.target != null) {
-                    this.recover(this.target);
+                if (this.target !== null && this.target !== undefined) {
+                    this.target.style.display = "visible";
                 } else {
                     console.log("Message on, but cannot find target element for facebook reels");
                 }
