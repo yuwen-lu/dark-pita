f1c4c24b7f2b8fdae60f3ee959b1adf7d7a1ee4d
Yuwen <yuwenlu@andrew.cmu.edu>
2022-08-19 01:57:41 -0400

    Fix facebook suggested for you hide issue

--- src/contents/components/facebook/suggested_for_you/facebook_suggested_for_you_hide.vue
+++ src/contents/components/facebook/suggested_for_you/facebook_suggested_for_you_hide.vue
@@ -1,89 +1,113 @@
 <template>
-    <div></div>
+  <div></div>
 </template>
 <script>
 export default {
-    data() { },
-    methods: {
-        remove(selectors) {
-            selectors.removeNode = [];
-            if (selectors.length != undefined) {
-                let len = selectors.length;
-                for (let i = 0; i < len; i++) {
-                    selectors.removeNode.push({
-                        parent: selectors[i].parentNode,
-                        inner: selectors[i].outerHTML,
-                        next: selectors[i].nextSibling
-                    });
-                }
-                for (let i = 0; i < len; i++)
-                    selectors[i].parentNode.removeChild(selectors[i]);
-            } else {
-                selectors.removeNode.push({
-                    parent: selectors.parentNode,
-                    inner: selectors.outerHTML,
-                    next: selectors.nextSibling
-                });
-                selectors.parentNode.removeChild(selectors);
-            }
-        },
-        recover(selectors) {
-            let len = selectors.removeNode.length;
-            for (let i = 0; i < len; i++) {
-                let node = selectors.removeNode[i];
-                if (node.next == null) node.parent.innerHTML += node.inner;
-                else {
-                    let div = document.createElement('div');
-                    div.innerHTML = node.inner;
-                    node.parent.insertBefore(div.childNodes[0], node.next);
-                }
-            }
+  data() {
+    return {
+      targetList: [],
+      isHideOn: false,
+    };
+  },
+  methods: {
+    getTargetList() {
+      let elementList = [];
+      let element;
+      // look for the sponsored component
+      var retrievedHtmls = document.getElementsByTagName("span");
+      for (var j = 0; j < retrievedHtmls.length; j++) {
+        if (retrievedHtmls[j].innerHTML.indexOf("Suggested for you") != -1 || retrievedHtmls[j].innerHTML.indexOf("Promoted by Facebook, Not Followed By You") ) {
+          var parentLevel = 7;
+          element = retrievedHtmls[j];
+          for (var k = 0; k < parentLevel; k++) {
+            if (element.parentElement == null) break;
+            element = element.parentElement;
+          }
+          elementList.push(element);
+          console.log("element found");
         }
+      }
+
+      if (elementList.length !== 0) {
+        this.targetList = elementList;
+      }
     },
-    mounted() {
-        this.emitter.on('facebook_suggested_for_you_hide', (message) => {
-
-            console.log("Received emitter message, " + message);
-
-            let elementList = [];
-            let element;
-            // look for the sponsored component
-            var retrievedHtmls = document.getElementsByTagName("span");
-            for (var j = 0; j < retrievedHtmls.length; j++) {
-                if (retrievedHtmls[j].innerHTML.indexOf("Suggested for you") != -1 || retrievedHtmls[j].innerHTML.indexOf("Promoted by Facebook") != -1) {
-                    // very ugly way, but the whole container is the 11th parent of the a tag
-                    var parentLevel = 7;
-                    element = retrievedHtmls[j];
-                    for (var k = 0; k < parentLevel; k++) {
-                        element = element.parentElement;
-                    }
-                    elementList.push(element);
-                }
-            }
-
-            if (message === 'on') {
-                console.log('facebook suggested for you content hide on');
-                this.sendAction(1, 'toggle facebook_suggested_for_you_hide');
-
-                if (elementList.length > 0) {
-                    this.target = elementList;
-                    this.remove(this.target);
-                    console.log(this.target + " removed");
-                } else {
-                    console.log("cannot find target element for facebook suggested for you content");
-                }
-            } else if (message === 'off') {
-                console.log('facebook suggested for you content hide off');
-                this.sendAction(0, 'toggle facebook_suggested_for_you_hide');
-                console.log("this.target: ", this.target);
-                if (this.target.length > 0) {
-                    this.recover(this.target);
-                    console.log(this.target + " restored");
-                }
-            }
-            this.$emit('update');
+    remove(selectors) {
+      selectors.removeNode = [];
+      if (selectors.length != undefined) {
+        let len = selectors.length;
+        for (let i = 0; i < len; i++) {
+          selectors.removeNode.push({
+            parent: selectors[i].parentNode,
+            inner: selectors[i].outerHTML,
+            next: selectors[i].nextSibling,
+          });
+        }
+        for (let i = 0; i < len; i++)
+          selectors[i].parentNode.removeChild(selectors[i]);
+      } else {
+        selectors.removeNode.push({
+          parent: selectors.parentNode,
+          inner: selectors.outerHTML,
+          next: selectors.nextSibling,
         });
-    }
+        selectors.parentNode.removeChild(selectors);
+      }
+    },
+    recover(selectors) {
+      let len = selectors.removeNode.length;
+      for (let i = 0; i < len; i++) {
+        let node = selectors.removeNode[i];
+        if (node.next == null) node.parent.innerHTML += node.inner;
+        else {
+          let div = document.createElement("div");
+          div.innerHTML = node.inner;
+          node.parent.insertBefore(div.childNodes[0], node.next);
+        }
+      }
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", () => {
+      if (this.isHideOn) {
+        this.getTargetList();
+        if (this.targetList.length !== 0) {
+          this.remove(this.targetList);
+        }
+      }
+    });
+
+    this.emitter.on("facebook_suggested_for_you_hide", (message) => {
+      console.log("Received emitter message, " + message);
+
+      this.getTargetList();
+
+      if (message === "on") {
+        this.isHideOn = true;
+        console.log("facebook suggested for you content hide on");
+        this.sendAction(1, "toggle facebook_suggested_for_you_hide");
+
+        if (this.targetList.length > 0) {
+          this.remove(this.targetList);
+          console.log(this.targetList + " removed");
+        } else {
+          console.log(
+            "cannot find target element for facebook suggested for you content"
+          );
+        }
+      } else if (message === "off") {
+        this.isHideOn = false;
+        console.log("facebook suggested for you content hide off");
+        this.sendAction(0, "toggle facebook_suggested_for_you_hide");
+        console.log("this.targetList: ", this.targetList);
+        if (this.targetList.length > 0) {
+          this.recover(this.targetList);
+          console.log(this.targetList + " restored");
+        }
+      }
+      this.$emit("update");
+    });
+  },
 };
 </script>
 <style lang=""></style>
--- src/contents/components/facebook/suggested_for_you/facebook_suggested_for_you_highlight.vue
+++ src/contents/components/facebook/suggested_for_you/facebook_suggested_for_you_highlight.vue
@@ -1,61 +1,104 @@
 <template>
-    <div></div>
+  <div></div>
 </template>
 <script>
 export default {
-    data() { },
-    methods: { },
-    mounted() {
-        this.emitter.on('facebook_suggested_for_you_highlight', (message) => {
+  data() {
+    return {
+      targetList: [],
+      isHighlightOn: false,
+    };
+  },
+  methods: {
+    getTargetList() {
+      let elementList = [];
+      let element;
+      // look for the sponsored component
+      var retrievedHtmls = document.getElementsByTagName("span");
+      for (var j = 0; j < retrievedHtmls.length; j++) {
+        if (
+          retrievedHtmls[j].innerHTML.indexOf("Suggested for you") != -1 ||
+          retrievedHtmls[j].innerHTML.indexOf("Promoted by Facebook") != -1
+        ) {
+          element = retrievedHtmls[j];
+          elementList.push(element);
+          console.log("element found");
+        }
+      }
 
-            console.log("Received emitter message, " + message);
+      if (elementList.length !== 0) {
+        this.targetList = elementList;
+      }
+    },
+    highlightTargetList() {
+      if (this.targetList.length > 0) {
+        for (var i = 0; i < this.targetList.length; i++) {
+          var tempElement = this.targetList[i];
+          tempElement.style.color = "#2D88FF";
+          tempElement.style.fontWeight = "bold";
+          tempElement.style.fontSize = "1.5em";
+          tempElement.innerHTML = tempElement.innerHTML.replace(
+            "Suggested for you",
+            "Promoted by Facebook, Not Followed By You"
+          );
+          console.log(tempElement + " highlighted");
+        }
+      } else {
+        console.log(
+          "cannot find target element for facebook suggested for you content"
+        );
+      }
+    },
+    removeHighlightFromTargetList() {
+      if (this.targetList.length > 0) {
+        for (var i = 0; i < this.targetList.length; i++) {
+          var tempElement = this.targetList[i];
+          tempElement.style.color = "inherit";
+          tempElement.style.fontWeight = "inherit";
+          tempElement.style.fontSize = "inherit";
+          tempElement.innerHTML = tempElement.innerHTML.replace(
+            "Promoted by Facebook, Not Followed By You",
+            "Suggested for you"
+          );
+        }
+        console.log(this.targetList + " highlight removed");
+      } else {
+        console.log(
+          "remove highlight failed, cannot find target element for facebook suggested for you content"
+        );
+      }
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", () => {
+      if (this.isHighlightOn) {
+        this.getTargetList();
+        this.highlightTargetList();
+      }
+    });
 
-            let elementList = [];
-            let element;
-            // look for the sponsored component
-            var retrievedHtmls = document.getElementsByTagName("span");
-            for (var j = 0; j < retrievedHtmls.length; j++) {
-                if (retrievedHtmls[j].innerHTML.indexOf("Suggested for you") != -1 || retrievedHtmls[j].innerHTML.indexOf("Promoted by Facebook") != -1) {
-                    element = retrievedHtmls[j];
-                    elementList.push(element);
-                    console.log("element found");
-                }
-            }
+    this.emitter.on("facebook_suggested_for_you_highlight", (message) => {
+      console.log("Received emitter message, " + message);
 
-            if (message === 'on') {
-                console.log('facebook suggested for you content highlight on');
-                this.sendAction(1, 'toggle facebook_suggested_for_you_highlight');
-                console.log('elementList: ' + elementList);
-                if (elementList.length > 0) {
-                    for (var i = 0; i < elementList.length; i++) {
-                        var tempElement = elementList[i];
-                        tempElement.style.color = '#2D88FF';
-                        tempElement.style.fontWeight = 'bold';
-                        tempElement.style.fontSize = '1.5em';
-                        tempElement.innerHTML = tempElement.innerHTML.replace("Suggested for you", "Promoted by Facebook, Not Followed By You");
-                        console.log(tempElement + " highlighted");
-                    }
-                    this.$emit("update");
-                } else {
-                    console.log("cannot find target element for facebook suggested for you content");
-                }
-            } else if (message === 'off') {
-                console.log('facebook suggested for you content highlight off');
-                this.sendAction(0, 'toggle facebook_suggested_for_you_highlight');
-                if (elementList.length > 0) {
-                    for (var i = 0; i < elementList.length; i++) {
-                        var tempElement = elementList[i];
-                        tempElement.style.color = 'inherit';
-                        tempElement.style.fontWeight = 'inherit';
-                        tempElement.style.fontSize = 'inherit';
-                        tempElement.innerHTML = tempElement.innerHTML.replace("Promoted by Facebook, Not Followed By You", "Suggested for you");
-                    }
-                    console.log(this.target + " highlight removed");
-                    this.$emit("update");
-                }
-            }
-        });
-    }
+      this.getTargetList();
+      this.highlightTargetList();
+
+      if (message === "on") {
+        this.isHighlightOn = true;
+        console.log("facebook suggested for you content highlight on");
+        this.sendAction(1, "toggle facebook_suggested_for_you_highlight");
+        if (this.targetList.length !== 0) {
+          this.highlightTargetList();
+        }
+      } else if (message === "off") {
+        this.isHighlightOn = false;
+        this.removeHighlightFromTargetList();
+        console.log("facebook suggested for you content highlight off");
+        this.sendAction(0, "toggle facebook_suggested_for_you_highlight");
+      }
+      this.$emit("update");
+    });
+  },
 };
 </script>
 <style lang=""></style>
