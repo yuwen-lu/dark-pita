f6ce7c6b8dcd1cfc8c33213bd4f7234ad86bae8d
Yuewen <yuewenkapa@gmail.com>
2022-08-19 22:01:13 -0400

    netflix audio mute

--- src/contents/components/netflix/hugepreview/netflix_hugepreview_disable.vue
+++ src/contents/components/netflix/hugepreview/netflix_hugepreview_disable.vue
@@ -17,17 +17,29 @@ export default {
       let that = this;
       //   setTimeout(() => {
       if (message === "on") {
+        this.sendAction(1, "toggle netflix_hugepreview_disable");
+
         if (document.querySelector(".nfp") !== null) {
           document.querySelector(".nfp").remove();
         }
+
         // nfp
         const image = document.createElement("img");
         image.src = this.imgLink;
         console.log(image);
+        image.style.width = "100%";
         document
           .querySelector(".motion-background-component")
           .appendChild(image);
+        if (
+          document
+            .querySelector("[aria-label='Turn audio on']")
+            .getAttribute("data-uia") === "audio-toggle-unmuted"
+        ) {
+          document.querySelector("[aria-label='Turn audio on']").click();
+        }
       } else if (message === "off") {
+        this.sendAction(0, "toggle netflix_hugepreview_disable");
         alert(
           "If you want the Preview on Featured Content back, please click Reset. Thanks :D"
         );
