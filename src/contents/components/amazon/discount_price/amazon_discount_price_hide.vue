6eeb64933207c78105acadcfbe5b58659da2334c
Yuwen <ylu23@nd.edu>
2023-04-18 22:01:08 -0400

    Fix amazon_discount_price_hide issues

--- src/contents/components/amazon/discount_price/amazon_discount_price_hide.vue
+++ src/contents/components/amazon/discount_price/amazon_discount_price_hide.vue
@@ -12,12 +12,12 @@ export default {
   methods: {},
   mounted() {
     this.emitter.on('amazon_discount_price_hide', (message) => {
-      this.target = document.querySelectorAll('[id^=corePriceDisplay_desktop_feature_div]')[0];
-      this.price = document.getElementsByClassName('priceToPay')[0];
+      this.target = document.querySelectorAll('[id^=apex_desktop]')[0];
+      this.price = document.getElementsByClassName("apexPriceToPay")[0];
       this.targetSibling = document.getElementById('desktop_unifiedPrice');
       this.priceSibling = document.getElementById(
-        'jewelryPriceBreakup_feature_div'
-      ).parentNode;
+        'bundleLTBSSavings_feature_div'
+      ).parentNode.parentNode.parentNode;
 
       if (
         this.target !== null &&
@@ -29,16 +29,16 @@ export default {
           console.log('amazon discount price hide on');
           this.sendAction(1, 'toggle amazon_discount_price_hide');
           this.targetSibling.parentNode.insertBefore(
-            this.price,
-            this.targetSibling.nextSibling
+            this.price.parentNode.parentNode,
+            this.targetSibling
           );
           this.target.style.display = 'none';
         } else if (message === 'off') {
           console.log('amazon discount price hide off');
           this.sendAction(0, 'toggle amazon_discount_price_hide');
-          this.targetSibling.parentNode.removeChild(this.price);
+          this.targetSibling.parentNode.removeChild(this.price.parentNode.parentNode);
           this.priceSibling.parentNode.insertBefore(
-            this.price,
+            this.price.parentNode.parentNode,
             this.priceSibling
           );
           this.target.style.display = 'block';
